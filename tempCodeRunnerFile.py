77fd509829dbcbb3f76cd3bcc3c00172f1478d7a
binitworld <contactbinitbhushan@gmail.com>
2023-09-09 04:44:14 +0530

    some initial edits

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,26 +1,12 @@
-# Lets talks about String slicing 
-names="King,Coder"
-print(names[0:4])
+# Write a program  to check character is any alphabet and check whether it is vowel or constant
+a=input("Enter your choice:")
 
-# For finding lenth of the string we uses the len function 
+alphabets ='abcdefghijklmnopqrstuvwxyzABCDEFIJKLMNOPQRSTUVWXYZ'
+vowel = "aeiouAEIOU"
 
-print(len(names))
-
-#so comma',' is also a string type 
-
-# take an example
-
-animals="Ceetah,zebra,cow,dog"
-len1=len(animals)
-print("For all animals",len1 , "letter words")
-print(len1)
-# see the example of  slicing in python /string 
-print(animals[:6])
-print(animals[:])
-
-# 2. solve one question {Take an user input and use for loop and print that input i outpt of specific character}
-Userinput=input("Enter your chooised Alphabets:\n ")
-
-for i in Userinput:
-    print(i)
-
+if a in alphabets:
+    print("Yes your choice is an Alphabet thank you!") 
+elif a in vowel:
+    print("and it is a case of Vowel...")
+else:
+    print("It is a consonant")
