6cc266e989aa9240d8094478d87ed34dca2e1e53
binitworld <contactbinitbhushan@gmail.com>
2023-09-12 04:18:45 +0530

    12.2

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,10 @@
-# Write a program  to check character is any alphabet and check whether it is vowel or constant
-a=input("Enter your choice:")
-
-alphabets ='abcdefghijklmnopqrstuvwxyzABCDEFIJKLMNOPQRSTUVWXYZ'
-vowel = "aeiouAEIOU"
-
-if a in alphabets:
-    print("Yes your choice is an Alphabet thank you!") 
-elif a in vowel:
-    print("and it is a case of Vowel...")
-else:
-    print("It is a consonant")
+# 5 write a program to print the facti=orial of a number enter by an user
+user=int(input("Enter the integer:"))
+f=1
+v=1
+while v <= user:
+    # print(v,end=",")
+    f = f*v
+    v = v+1
+print("The factorial of your input is :",f)
+# 5.1 practice set
