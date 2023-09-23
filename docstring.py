66c17821b850a0a33b7644c366dff3c6b5a15795
binitworld <contactbinitbhushan@gmail.com>
2023-09-23 15:57:24 +0530

    23.5

---
+++ docstring.py
@@ -0,0 +1,19 @@
+# If there we had written som type of function the doc string just help us to understand that function 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ opener.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+url = input("Enter the url/:")
+
+for i in range(34):
+    webbrowser.open(url)
