e6b753a6313104f12420f475a6a8bdd903147aac
binitworld <contactbinitbhushan@gmail.com>
2023-10-06 04:30:52 +0530

    multiple time msg print code

---
+++ emojii.py
@@ -0,0 +1,12 @@
+# Define the emoji as a string
+emoji = " 🫶🏻 "
+
+# Specify the number of times you want to print the emoji
+num_times = 1000
+
+# Use a for loop to print the emoji multiple times
+for _ in range(num_times):
+    print(emoji, end=" ")  # Use end=" " to print on the same line
+
+# Add a newline character at the end to separate lines
+print()
