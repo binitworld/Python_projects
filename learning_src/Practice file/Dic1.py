4c440d9ce26611c1dba9e3a8b4e4bf2d1894a07f
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-18 03:48:45 +0530

    p1 dic

---
+++ learning_src/Practice file/Dic1.py
@@ -0,0 +1,9 @@
+# Accessing The Dictionary items
+
+info = {'name':'Miraj', 'age' : '23', 'Eligible to Drink' : True}
+for info, doc in info.items():
+    print(f"{info}:{doc}")
+
+
+
+# Write a function that takes a dictionary as input and returns a list of all the keys in the dictionary.
