7fe4d0aaf1340706c032fd1f409d15c1a1d94e3d
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-19 01:32:42 +0530

    19-10-3

---
+++ learning_src/Practice file/Methods of dic/pop_method-dic.py
@@ -0,0 +1,41 @@
+#Here is AN Example of pop method in this i usually pop the user id according to user id 
+
+Office = {
+    'User1': 67, 'User2':77, 'User3':34,'User4' : 98, 'User5': 78, 'User6': 98, 'User7': 99, 'User8':11, 'User9': 108
+}
+
+print(Office)
+Office.pop('User7')
+print(Office)
+Office.pop('User1')
+print(Office)
+Office.pop('User2')
+print(Office)
+Office.pop('User3')
+print(Office)
+Office.pop('User4')
+print(Office)
+Office.pop('User5')
+print(Office)
+Office.pop('User8')
+print(Office)
+Office.pop('User6')
+print(Office)
+Office.pop('User9')
+print(Office)
+
+# Define a dictionary with model names and numbers
+Iphone = {
+    'Model a': 1, 'Model b': 2, 'Model c': 3, 'Model d': 4,
+    'Model e': 5, 'Model f': 6, 'Model g': 7, 'Model h': 8,
+    'Model i': 9, 'Model j': 10, 'Model k': 11, 'Model l': 12,
+    'Model m': 13, 'Model n': 14, 'Model o': 15
+}
+
+print(Iphone)
+Iphone.popitem()
+print(Iphone)
+
+
+
+
