e6c426b9b73ebb764251e664a6f0c034ec49023d
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-19 01:50:30 +0530

    19-10-7

---
+++ learning_src/Practice file/Methods of dic/del-method_dic.py
@@ -0,0 +1,13 @@
+Profile={
+    "Insta":"Ram", "Twitter":"Muskan","Github":"Hitesh","Facebook":"Riya"
+}
+print(Profile)
+
+del Profile["Twitter"]
+print(Profile)
+del Profile["Insta"]
+print(Profile)
+del Profile["Facebook"]
+print(Profile)
+del Profile["Github"]
+print(Profile)
