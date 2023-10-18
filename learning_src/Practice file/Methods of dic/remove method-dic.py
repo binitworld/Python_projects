d84fd2a9938fc172e41dff38c3baa99e07b9f97d
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-19 01:32:08 +0530

    19-10- 1

---
+++ learning_src/Practice file/Methods of dic/remove method-dic.py
@@ -0,0 +1,10 @@
+Books ={
+    "Maths" : "Rs Agarwal","Science":"NCERt","General Knowledge ":"Ms. Laxmi Kanth"
+}
+New_Books={"Hindi":"Varnmala", "English":"Litreature"}
+
+Books.update(New_Books)
+print (Books)
+
+Books.clear()
+print(Books)
